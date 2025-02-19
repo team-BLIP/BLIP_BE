e4c5f3a31a1a000c2c691932721b55ca6ab03389
sorim <enhld00@gmail.com>
2025-02-19 18:14:51 +0900

    refactor : 서명 방식 변경

--- src/main/java/com/example/blip_be/global/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/blip_be/global/security/jwt/JwtTokenProvider.java
@@ -6,8 +6,10 @@ import com.example.blip_be.global.exception.ExpiredTokenException;
 import com.example.blip_be.global.exception.InvalidTokenException;
 import com.example.blip_be.global.security.auth.AuthDetailsService;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,6 +19,8 @@ import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 import java.util.Date;
 
 @Component
@@ -29,19 +33,24 @@ public class JwtTokenProvider {
     private final AuthDetailsService authDetailsService;
     private final RefreshTokenRepository refreshTokenRepository;
 
+    private SecretKeySpec secretKeySpec;
+
+    @PostConstruct
+    public void initSecretKeySpec() {
+        this.secretKeySpec = new SecretKeySpec(jwtProperties.getSecretKey().getBytes(), SignatureAlgorithm.HS256.getJcaName());
+    }
+
     public String generateAccessToken(String accountId) {
         String token = generateToken(accountId, "access", jwtProperties.getAccessExp());
-        logger.info("계정 ID: {}에 대해 액세스 토큰 생성됨: {}", accountId, token);
         return token;
     }
 
     public String generateRefreshToken(String accountId) {
         String refreshToken = generateToken(accountId, "refresh", jwtProperties.getRefreshExp());
-        logger.info("계정 ID: {}에 대해 리프레시 토큰 생성됨: {}", accountId, refreshToken);
-
         refreshTokenRepository.save(RefreshToken.builder()
                 .accountId(accountId)
                 .token(refreshToken)
+                .ttl(jwtProperties.getRefreshExp())
                 .build());
         return refreshToken;
     }
@@ -49,7 +58,7 @@ public class JwtTokenProvider {
     public String generateToken(String accountId, String type, Long exp) {
         logger.debug("계정 ID: {}에 대해 토큰 생성 중. 유형: {}, 만료 시간: {}초", accountId, type, exp);
         return Jwts.builder()
-                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .signWith(secretKeySpec)
                 .setSubject(accountId)
                 .claim("type", type)
                 .setIssuedAt(new Date())
@@ -59,41 +68,30 @@ public class JwtTokenProvider {
 
     public String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader(jwtProperties.getHeader());
-        if (bearerToken != null && bearerToken.startsWith(jwtProperties.getPrefix())) {
-            String token = bearerToken.replace(jwtProperties.getPrefix(), "");
-            logger.info("해당 토큰 해석됨: {}", token);
-            return token;
-        }
-        logger.warn("요청 헤더에서 베어러 토큰을 찾을 수 없거나 잘못된 형식임");
-        return null;
+        return parseToken(bearerToken);
     }
 
     public Authentication authentication(String token) {
-        String subject = getTokenSubject(token);
-        logger.info("계정 ID: {}로 인증 중", subject);
-
-        UserDetails userDetails = authDetailsService.loadUserByUsername(subject);
+        UserDetails userDetails = authDetailsService.loadUserByUsername(token);
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
-    public Claims parseToken(String token) {
-        try {
-            return Jwts.parserBuilder()
-                    .setSigningKey(jwtProperties.getSecretKey())
-                    .build()
-                    .parseClaimsJws(token)
-                    .getBody();
-        } catch (Exception e) {
-            throw new RuntimeException("JWT token is invalid", e);
+    public String parseToken(String bearerToken) {
+
+        if (bearerToken != null && bearerToken.startsWith(jwtProperties.getPrefix())) {
+            return bearerToken.replace(jwtProperties.getPrefix(), "").trim();
         }
+        return null;
     }
 
     private Claims getTokenBody(String token) {
         try {
-            System.out.println("토큰: " + token);
-            return Jwts.parser().setSigningKey(jwtProperties.getSecretKey())
-                    .parseClaimsJws(token).getBody();
-        } catch (ExpiredTokenException e) {
+            return Jwts.parserBuilder()
+                    .setSigningKey(secretKeySpec)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
             throw ExpiredTokenException.EXCEPTION;
         } catch (Exception e) {
             throw InvalidTokenException.EXCEPTION;
@@ -101,13 +99,6 @@ public class JwtTokenProvider {
     }
 
     private String getTokenSubject(String token) {
-        try {
-            String subject = getTokenBody(token).getSubject();
-            logger.debug("토큰에서 추출된 계정 ID: {}", subject);
-            return subject;
-        } catch (Exception e) {
-            logger.error("토큰에서 계정 ID 추출 오류: {}", e.getMessage());
-            throw InvalidTokenException.EXCEPTION;
-        }
+        return getTokenBody(token).getSubject();
     }
 }
