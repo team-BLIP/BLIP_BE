bdc2f7bd2fa1a15afd5900b3fc18d54c2e5317e8
sorim <enhld00@gmail.com>
2025-02-24 23:07:36 +0900

    refactor : 유저 조회 방식 변경

--- src/main/java/com/example/blip_be/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/blip_be/domain/user/domain/repository/UserRepository.java
@@ -1,16 +1,14 @@
 package com.example.blip_be.domain.user.domain.repository;
 
-import com.example.blip_be.domain.user.domain.User;
+import com.example.blip_be.domain.user.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    Optional<User> findByAccountId(String accountId);
-
-    Optional<User> findByEmail(String email);
-    Boolean existsByAccountId(String accountId);
+    Optional<UserEntity> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
