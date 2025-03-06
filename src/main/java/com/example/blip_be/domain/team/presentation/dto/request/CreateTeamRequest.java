73c8c290a242110b44e32ad0b6bd73c5be46a568
sorim <enhld00@gmail.com>
2025-03-06 11:00:21 +0900

    feat : CreateTeamDTO 추가

---
+++ src/main/java/com/example/blip_be/domain/team/presentation/dto/request/CreateTeamRequest.java
@@ -0,0 +1,15 @@
+package com.example.blip_be.domain.team.presentation.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class  CreateTeamRequest {
+
+    @NotNull(message = "3 ~ 20자까지 가능합니다.")
+    private String teamName;
+
+    private String nickName;
+}
