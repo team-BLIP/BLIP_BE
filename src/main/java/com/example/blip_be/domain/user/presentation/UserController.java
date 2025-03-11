c2c3cb9e6d36270305f53785f9aff20d1fdf9723
sorim0626 <enhld00@gmail.com>
2025-03-12 08:53:28 +0900

    refactor : 회원 가입 방식 수정

--- src/main/java/com/example/blip_be/domain/user/presentation/UserController.java
+++ src/main/java/com/example/blip_be/domain/user/presentation/UserController.java
@@ -5,6 +5,7 @@ import com.example.blip_be.domain.user.presentation.dto.request.LoginRequest;
 import com.example.blip_be.domain.user.presentation.dto.request.SignUpRequest;
 import com.example.blip_be.domain.user.service.LoginService;
 import com.example.blip_be.domain.user.service.SignUpService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -20,18 +21,13 @@ public class UserController {
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
-    public void signUp(
-            @RequestPart("account_id") String accountId,
-            @RequestPart("password") String password,
-            @RequestPart("email") String email
-    ) {
-        SignUpRequest request = new SignUpRequest(accountId, password, email);
+    public void signUp(@RequestBody @Valid SignUpRequest request) {
         signUpService.registerUser(request);
     }
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
-    public TokenResponse login(@RequestBody LoginRequest request) {
+    public TokenResponse login(@RequestBody @Valid LoginRequest request) {
         return loginService.login(request);
     }
 }
