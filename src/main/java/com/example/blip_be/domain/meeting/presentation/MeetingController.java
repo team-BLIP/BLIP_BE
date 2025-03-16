04c89e8edb367ce49612f0c2eac1e7431e0c6677
sorim0626 <enhld00@gmail.com>
2025-03-16 22:43:00 +0900

    feat : 회의 컨트롤러 추가

---
+++ src/main/java/com/example/blip_be/domain/meeting/presentation/MeetingController.java
@@ -0,0 +1,54 @@
+package com.example.blip_be.domain.meeting.presentation;
+
+import com.example.blip_be.domain.meeting.domain.Meeting;
+import com.example.blip_be.domain.meeting.presentation.dto.request.EndMeetingRequest;
+import com.example.blip_be.domain.meeting.presentation.dto.request.JoinMeetingRequest;
+import com.example.blip_be.domain.meeting.presentation.dto.request.LeaveMeetingRequest;
+import com.example.blip_be.domain.meeting.presentation.dto.request.StartMeetingRequest;
+import com.example.blip_be.domain.meeting.presentation.dto.response.EndMeetingResponse;
+import com.example.blip_be.domain.meeting.presentation.dto.response.JoinMeetingResponse;
+import com.example.blip_be.domain.meeting.presentation.dto.response.LeaveMeetingResponse;
+import com.example.blip_be.domain.meeting.presentation.dto.response.StartMeetingResponse;
+import com.example.blip_be.domain.meeting.service.EndMeetingService;
+import com.example.blip_be.domain.meeting.service.JoinMeetingService;
+import com.example.blip_be.domain.meeting.service.LeaveMeetingService;
+import com.example.blip_be.domain.meeting.service.StartMeetingService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/meetings")
+@RequiredArgsConstructor
+public class MeetingController {
+
+    private final StartMeetingService startMeetingService;
+    private final JoinMeetingService joinMeetingService;
+    private final LeaveMeetingService leaveMeetingService;
+    private final EndMeetingService endMeetingService;
+
+    @PostMapping("/start")
+    @ResponseStatus(HttpStatus.OK)
+    public StartMeetingResponse startMeeting(@RequestBody @Valid StartMeetingRequest request) {
+        return startMeetingService.startMeeting(request);
+    }
+
+    @PostMapping("/join")
+    @ResponseStatus(HttpStatus.OK)
+    public JoinMeetingResponse joinMeeting(@RequestBody @Valid JoinMeetingRequest request) {
+        return joinMeetingService.joinMeeting(request);
+    }
+
+    @PostMapping("/leave")
+    @ResponseStatus(HttpStatus.OK)
+    public LeaveMeetingResponse leaveMeeting(@RequestBody @Valid LeaveMeetingRequest request) {
+        return leaveMeetingService.leaveMeeting(request);
+    }
+
+    @PostMapping("/end")
+    public EndMeetingResponse endMeeting(@RequestBody EndMeetingRequest request, MultipartFile file) {
+        return endMeetingService.endMeeting(request, file);
+    }
+}
