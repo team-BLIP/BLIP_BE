363fe7470c5bb7cd9a9916f3b7eea8c20927c7f1
sorim0626 <enhld00@gmail.com>
2025-03-18 19:05:36 +0900

    chore : url 수정

--- src/main/java/com/example/blip_be/global/config/WebSocketConfig.java
+++ src/main/java/com/example/blip_be/global/config/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(signalingSocketHandler(), "/call").setAllowedOrigins("*");
+        registry.addHandler(signalingSocketHandler(), "/signaling").setAllowedOrigins("*");
     }
 
     @Bean
