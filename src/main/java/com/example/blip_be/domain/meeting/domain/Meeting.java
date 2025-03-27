ef2e7a57929b4acc2d2185f11471448c82659ed8
sorim0626 <enhld00@gmail.com>
2025-03-27 10:27:35 +0900

    feat : User 필드 추가

--- src/main/java/com/example/blip_be/domain/meeting/domain/Meeting.java
+++ src/main/java/com/example/blip_be/domain/meeting/domain/Meeting.java
@@ -1,6 +1,7 @@
 package com.example.blip_be.domain.meeting.domain;
 
 import com.example.blip_be.domain.team.domain.Team;
+import com.example.blip_be.domain.user.domain.UserEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotNull;
@@ -8,6 +9,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.catalina.User;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -37,6 +39,10 @@ public class Meeting {
     @JoinColumn(name = "team_id")
     private Team team;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
     @OneToMany(mappedBy = "meeting", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<MeetingFeedback> feedbacks = new ArrayList<>();
 
@@ -49,12 +55,13 @@ public class Meeting {
     private boolean isStarted = false;
 
     @Builder
-    public Meeting(String topic, LocalDateTime startTime, LocalDateTime endTime, Team team, String fileUrl) {
+    public Meeting(String topic, LocalDateTime startTime, LocalDateTime endTime, Team team, String fileUrl, UserEntity user) {
         this.topic = topic;
         this.startTime = startTime;
         this.endTime = endTime;
         this.team = team;
         this.fileUrl = fileUrl;
+        this.user = user;
     }
 
     @AssertTrue(message = "종료 시간은 시작 시간보다 뒤여야 합니다.")
