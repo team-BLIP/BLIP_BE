0e65b55f574119e047dcadca53cd7fee876155d7
sorim0626 <enhld00@gmail.com>
2025-03-27 10:28:16 +0900

    .

--- src/main/java/com/example/blip_be/domain/meeting/domain/repository/MeetingRepository.java
+++ src/main/java/com/example/blip_be/domain/meeting/domain/repository/MeetingRepository.java
@@ -1,6 +1,7 @@
 package com.example.blip_be.domain.meeting.domain.repository;
 
 import com.example.blip_be.domain.meeting.domain.Meeting;
+import com.example.blip_be.domain.meeting.domain.MeetingParticipation;
 import com.example.blip_be.domain.user.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
