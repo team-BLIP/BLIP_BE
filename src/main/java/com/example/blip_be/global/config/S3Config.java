a64d7d0017768b60858bf5e095fac382b3dd5654
sorim0626 <enhld00@gmail.com>
2025-03-30 20:13:49 +0900

    refactor : S3 코드 리팩토링

--- src/main/java/com/example/blip_be/global/config/S3Config.java
+++ src/main/java/com/example/blip_be/global/config/S3Config.java
@@ -2,6 +2,7 @@ package com.example.blip_be.global.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,9 +22,9 @@ public class S3Config {
     private String region;
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
+    public AmazonS3 amazonS3Client() {
         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+        return AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
