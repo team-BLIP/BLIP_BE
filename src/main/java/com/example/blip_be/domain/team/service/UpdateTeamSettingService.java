64fa6cbbf4733e83c6a0ebc7730498bf78e0fb8a
sorim0626 <enhld00@gmail.com>
2025-04-03 09:06:02 +0900

    feat : 팀 이름 변경 서비스 추가

---
+++ src/main/java/com/example/blip_be/domain/team/service/UpdateTeamSettingService.java
@@ -0,0 +1,32 @@
+package com.example.blip_be.domain.team.service;
+
+import com.example.blip_be.domain.team.TeamNotFoundException;
+import com.example.blip_be.domain.team.domain.Team;
+import com.example.blip_be.domain.team.domain.repository.TeamRepository;
+import com.example.blip_be.domain.team.presentation.dto.request.UpdateTeamSettingRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UpdateTeamSettingService {
+
+    private final TeamRepository teamRepository;
+
+    @Transactional
+    public void updateTeam(Long teamId, UpdateTeamSettingRequest request) {
+
+        if (request.getNewName() == null) {
+            throw new IllegalArgumentException("팀 이름은 비어있을 수 없습니다.");
+        }
+
+        Team team = teamRepository.findById(teamId)
+                        .orElseThrow(()-> TeamNotFoundException.EXCEPTION);
+
+        System.out.println("Before update: " + team.getTeamName());
+        team.updateTeam(request.getNewName());
+        System.out.println("After update: " + team.getTeamName());
+
+    }
+}
