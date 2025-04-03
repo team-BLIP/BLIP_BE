b743e180dd466f890101f84a2f5fecc69d8b60bd
sorim0626 <enhld00@gmail.com>
2025-04-03 09:08:00 +0900

    chore : 공백 삭제

--- src/main/java/com/example/blip_be/BlipBeApplication.java
+++ src/main/java/com/example/blip_be/BlipBeApplication.java
@@ -4,13 +4,12 @@ import com.example.blip_be.global.security.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableConfigurationProperties(JwtProperties.class)
 public class BlipBeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BlipBeApplication.class, args);
     }
-
 }
