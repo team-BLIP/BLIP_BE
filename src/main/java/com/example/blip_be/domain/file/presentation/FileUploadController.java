2ba28f88b096c8128c8fa52b1078055528f020d9
sorim0626 <enhld00@gmail.com>
2025-04-06 21:27:42 +0900

    feat : presignedUrl 추가

--- src/main/java/com/example/blip_be/domain/file/presentation/FileUploadController.java
+++ src/main/java/com/example/blip_be/domain/file/presentation/FileUploadController.java
@@ -2,6 +2,7 @@ package com.example.blip_be.domain.file.presentation;
 
 import com.example.blip_be.domain.file.presentation.dto.response.FileUploadResponse;
 import com.example.blip_be.domain.file.service.FileUploadService;
+import com.example.blip_be.global.ai.service.PresignedUrlService;
 import com.example.blip_be.global.error.CustomException;
 import com.example.blip_be.global.error.ErrorCode;
 import com.example.blip_be.global.error.ErrorResponse;
@@ -15,6 +16,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,12 +26,19 @@ import java.util.Map;
 public class FileUploadController {
 
     private final FileUploadService fileUploadService;
+    private final PresignedUrlService presignedUrlService;
 
     @PostMapping("/audio")
     @ResponseStatus(HttpStatus.CREATED)
-    public FileUploadResponse uploadVoice(@RequestParam("file") MultipartFile file){
+    public FileUploadResponse uploadAudio(@RequestParam("file") MultipartFile file){
         String filePath = fileUploadService.uploadAudioFile(file);
         return new FileUploadResponse("음성 파일 업로드 성공", filePath);
+    }
+
+    @GetMapping("/presigned-url")
+    public ResponseEntity<String> getPresignedUrl(@RequestParam String fileName) {
+        URL presignedUrl = presignedUrlService.generatePresignedUrl(fileName);
+        return ResponseEntity.ok(presignedUrl.toString());
     }
 
     @PostMapping("/image")
