cee2b783a02d0ade80e5df1fd00ab0da19559556
sorim0626 <enhld00@gmail.com>
2025-04-06 21:33:18 +0900

    refactor : url 변경

--- src/main/java/com/example/blip_be/global/ai/service/WebClientService.java
+++ src/main/java/com/example/blip_be/global/ai/service/WebClientService.java
@@ -1,9 +1,19 @@
 package com.example.blip_be.global.ai.service;
 
+import com.example.blip_be.domain.file.service.FileUploadService;
+import com.example.blip_be.domain.meeting.domain.Meeting;
+import com.example.blip_be.domain.meeting.domain.repository.MeetingRepository;
+import com.example.blip_be.domain.meeting.presentation.dto.request.EndMeetingRequest;
+import com.example.blip_be.domain.meeting.presentation.dto.response.EndMeetingResponse;
+import com.example.blip_be.domain.team.domain.Team;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -15,12 +25,12 @@ public class WebClientService {
         return webClientBuilder.baseUrl(baseUrl).build();
     }
 
-    public Mono<Result> analyzeMeeting(String meetingId, String fileUrl) {
-        WebClient webClient = createWebClient("http://192.168.1.60:8000/");
+    public Mono<Result> analyzeMeeting(String s3Url) {
+        WebClient webClient = createWebClient("http://3.35.180.21/");
 
         return webClient.post()
-                .uri("/analyze")
-                .bodyValue(new AIRequest(meetingId, fileUrl))
+                .uri("/meeting")
+                .bodyValue(new AIRequest(s3Url))
                 .retrieve()
                 .bodyToMono(Result.class);
     }
