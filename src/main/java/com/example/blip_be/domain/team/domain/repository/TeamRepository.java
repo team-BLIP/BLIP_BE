cfde34fd8a1f4acec855ee81fcf41289b0c0d0bd
sorim0626 <enhld00@gmail.com>
2025-04-08 16:09:33 +0900

    feat : 팀 리스트 메서드 추가

--- src/main/java/com/example/blip_be/domain/team/domain/repository/TeamRepository.java
+++ src/main/java/com/example/blip_be/domain/team/domain/repository/TeamRepository.java
@@ -4,17 +4,20 @@ import com.example.blip_be.domain.team.domain.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
-Optional<Team> findById(Long id);
+    Optional<Team> findById(Long id);
 
-Optional<Team> findByInviteLink(String inviteLink);
+    Optional<Team> findByInviteLink(String inviteLink);
 
-boolean existsById(Long id);
+    boolean existsById(Long id);
 
-void deleteById(Long id);
+    void deleteById(Long id);
+
+    List<Team> findByMembersUserId(Long userId);
 }
