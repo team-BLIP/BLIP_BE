57bb6f5448dccc77d7f2e5e0119266eaf329b8a1
sorim0626 <enhld00@gmail.com>
2025-04-08 16:07:56 +0900

    feat : 시간 추가

--- src/main/java/com/example/blip_be/global/ai/service/Result.java
+++ src/main/java/com/example/blip_be/global/ai/service/Result.java
@@ -2,12 +2,13 @@ package com.example.blip_be.global.ai.service;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
-import java.math.BigDecimal;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Result {
     private String summary;
     private String feedback;
+    private String time;
 }
