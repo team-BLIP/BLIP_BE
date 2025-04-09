0ca57ec3a3e21f7639182c8c74f8c3118dd8c895
sorim0626 <enhld00@gmail.com>
2025-04-10 01:01:10 +0900

    feat : 요약 response 추가

---
+++ src/main/java/com/example/blip_be/domain/meeting/presentation/dto/response/MeetingSummaryResponse.java
@@ -0,0 +1,12 @@
+package com.example.blip_be.domain.meeting.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MeetingSummaryResponse {
+
+    private Long meetingId;
+    private String summary;
+}
