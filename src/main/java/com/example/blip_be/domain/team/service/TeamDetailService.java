e6b7386f9d8ae0641129f5693c90dd672394311a
sorim0626 <enhld00@gmail.com>
2025-04-10 01:02:35 +0900

    feat : 피드백 삭제 및 요약 추가

--- src/main/java/com/example/blip_be/domain/team/service/TeamDetailService.java
+++ src/main/java/com/example/blip_be/domain/team/service/TeamDetailService.java
@@ -1,10 +1,9 @@
 package com.example.blip_be.domain.team.service;
 
-import com.amazonaws.services.kms.model.NotFoundException;
 import com.example.blip_be.domain.meeting.domain.Meeting;
 import com.example.blip_be.domain.meeting.domain.MeetingSummary;
 import com.example.blip_be.domain.meeting.domain.repository.MeetingRepository;
-import com.example.blip_be.domain.meeting.presentation.dto.response.MeetingResponse;
+import com.example.blip_be.domain.meeting.presentation.dto.response.MeetingSummaryResponse;
 import com.example.blip_be.domain.team.TeamNotFoundException;
 import com.example.blip_be.domain.team.domain.Team;
 import com.example.blip_be.domain.team.domain.repository.TeamRepository;
@@ -26,30 +25,25 @@ public class TeamDetailService {
         Team team = teamRepository.findById(teamId)
                 .orElseThrow(() -> TeamNotFoundException.EXCEPTION);
 
-        List<MeetingResponse> meetingResponses = meetingRepository
+        List<MeetingSummaryResponse> meetingSummaries = meetingRepository
                 .findAllByTeam(team)
                 .stream()
-                .map(this::convertToMeetingResponse)
+                .map(this::convertToMeetingSummaryResponse)
                 .toList();
 
         return new TeamDetailResponse(
                 team.getId(),
                 team.getTeamName(),
                 team.getImageUrl(),
-                meetingResponses
+                meetingSummaries
         );
     }
 
-    private MeetingResponse convertToMeetingResponse(Meeting meeting) {
+    private MeetingSummaryResponse convertToMeetingSummaryResponse(Meeting meeting) {
         String summaryContent = Optional.ofNullable(meeting.getMeetingSummary())
                 .map(MeetingSummary::getSummary)
                 .orElse(null);
 
-        String feedbackContent = Optional.ofNullable(meeting.getFeedbacks())
-                .filter(feedbacks -> !feedbacks.isEmpty())
-                .map(feedbacks -> feedbacks.get(0).getFeedback())
-                .orElse(null);
-
-        return new MeetingResponse(summaryContent, feedbackContent);
+        return new MeetingSummaryResponse(meeting.getId(), summaryContent);
     }
 }
